55fbfaca4ba8704716fa4c648cadb5bc8507313b
Aste <aste@tuta.io>
2022-07-22 09:12:58 +0200

    Add initial html/css form layout and js object construcotrs and functions

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A Booklist object constructor for the Library Project in the JavaScript Course of The Odin Project
+A book list object constructor for the Library Project in the JavaScript Course of The Odin Project
--- booklist.js
+++ booklist.js
@@ -1,18 +1,58 @@
-let myLibrary = []
+const myLibrary = []
 
-function Book(title, author, pages, read) {
-    // Book Object Constructor
+// Object Constructor Function
+function Book(title, author, pages) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
-    // It is better to create a function in the prototype of an object. That way the function
-    // is not recreated in all instances of the object, which matters if you have many instances
-    this.bookInfo = function () {
-        return (title, author, pages, read)
-    }
+    this.read = false
 }
 
-function addBookToLibrary() {
-
+// It is better to create a function in the prototype of an object. That way the function
+// is not recreated in all instances of the object, which matters if you have many instances
+Book.prototype.info = function () {
+    console.log(this.title, this.author, this.pages, this.read)
+    // return [this.title, this.author, this.pages, this.read]
 }
+
+
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages)
+    myLibrary.push(newBook)
+}
+
+
+addBookToLibrary("Brave New World", "Aldous Huxley", "311")
+addBookToLibrary("Nineteen Eighty-Four", "George Orwell", "328")
+addBookToLibrary("A Tale of Two Cities", "Charles Dickens", "448")
+addBookToLibrary("test", "Authorsen", "1000")
+
+
+
+
+// console.log(Object.entries(myLibrary));
+// console.log(Object.entries(Book.prototype));
+// myLibrary[0].info()
+// console.log(Book.prototype.info)
+// console.log(myLibrary[0].info)
+// // console.log(myLibrary[0].prototype.info.isPrototypeOf(Book))
+// console.log(myLibrary[0].constructor)
+// console.log(Object.keys(myLibrary[0]))
+// console.log(Object.values(myLibrary[0]))
+// console.log(Object.getPrototypeOf(myLibrary[0]))
+// for (const [key, value] of Object.entries(myLibrary[0])) {
+//     console.log(`${key}: ${value}`);
+// }
+// console.log("")
+// Object.entries(myLibrary[0]).forEach(([key, value]) => {
+//     console.log(`${key}: ${value}`)
+// });
+// console.log(myLibrary[0])
+
+myLibrary.forEach((element, index, array) => {
+    console.log(element); // 100, 200, 300
+    console.log(index); // 0, 1, 2
+    // console.log(array); // same myArray object 3 times
+    console.log(""); // same myArray object 3 times
+});
+// console.log(Object.getPrototypeOf(myLibrary[0]))
