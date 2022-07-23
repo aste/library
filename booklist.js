073e0a40ad72152c5c15a19237915d6499f8e328
Aste <aste@tuta.io>
2022-07-23 13:36:35 +0200

    Populate DOM elements with Book Object Properties

--- booklist.js
+++ booklist.js
@@ -1,58 +1,54 @@
 const myLibrary = []
+const bookList = document.querySelector('#bookList')
 
 // Object Constructor Function
 function Book(title, author, pages) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = false
+    this.read = "Unread"
+    this.remove = "Remove"
 }
 
 // It is better to create a function in the prototype of an object. That way the function
 // is not recreated in all instances of the object, which matters if you have many instances
 Book.prototype.info = function () {
-    console.log(this.title, this.author, this.pages, this.read)
-    // return [this.title, this.author, this.pages, this.read]
+    return [this.title, this.author, this.pages, this.read]
 }
 
-
-function addBookToLibrary(title, author, pages) {
+// Create new book object instance from the object constructor function and push it to myLibrary
+function addBookToMyLibrary(title, author, pages) {
     const newBook = new Book(title, author, pages)
     myLibrary.push(newBook)
 }
 
+// Update DOM Library, with an input library type of array containing objects
+function updateDomLibrary(lib) {
+    const li = document.createElement('li')
 
-addBookToLibrary("Brave New World", "Aldous Huxley", "311")
-addBookToLibrary("Nineteen Eighty-Four", "George Orwell", "328")
-addBookToLibrary("A Tale of Two Cities", "Charles Dickens", "448")
-addBookToLibrary("test", "Authorsen", "1000")
+    // Loop through the array of book objects in library
+    for (let i = 0; i < lib.length; i++) {
+        // Loop through book object, assign element type, text, attributes and append to DOM
+        for (const [key, val] of Object.entries(lib[i])) {
+            if (key == "title" || key == "author" || key == "pages") {
+                const currentElement = document.createElement('span')
+                currentElement.textContent = `${val}`;
+                currentElement.setAttribute("class", key);
+                li.appendChild(currentElement)
+            }
+            if (key == "read" || key == "remove") {
+                const currentElement = document.createElement('button')
+                currentElement.textContent = `${val}`;
+                currentElement.setAttribute("class", key);
+                li.appendChild(currentElement)
+            }
+        }
+    }
+    bookList.appendChild(li)
+}
 
+addBookToMyLibrary("Brave New World", "Aldous Huxley", "311")
+addBookToMyLibrary("Nineteen Eighty-Four", "George Orwell", "328")
+addBookToMyLibrary("A Tale of Two Cities", "Charles Dickens", "448")
 
-
-
-// console.log(Object.entries(myLibrary));
-// console.log(Object.entries(Book.prototype));
-// myLibrary[0].info()
-// console.log(Book.prototype.info)
-// console.log(myLibrary[0].info)
-// // console.log(myLibrary[0].prototype.info.isPrototypeOf(Book))
-// console.log(myLibrary[0].constructor)
-// console.log(Object.keys(myLibrary[0]))
-// console.log(Object.values(myLibrary[0]))
-// console.log(Object.getPrototypeOf(myLibrary[0]))
-// for (const [key, value] of Object.entries(myLibrary[0])) {
-//     console.log(`${key}: ${value}`);
-// }
-// console.log("")
-// Object.entries(myLibrary[0]).forEach(([key, value]) => {
-//     console.log(`${key}: ${value}`)
-// });
-// console.log(myLibrary[0])
-
-myLibrary.forEach((element, index, array) => {
-    console.log(element); // 100, 200, 300
-    console.log(index); // 0, 1, 2
-    // console.log(array); // same myArray object 3 times
-    console.log(""); // same myArray object 3 times
-});
-// console.log(Object.getPrototypeOf(myLibrary[0]))
+updateDomLibrary(myLibrary)
