db69707789a74469752eaf1bb105de11a36b8b43
Aste <aste@tuta.io>
2022-07-25 12:27:10 +0200

    Remove button, removes target book from DOM and myLibrary array

--- booklist.js
+++ booklist.js
@@ -1,7 +1,10 @@
 const myLibrary = []
 const bookList = document.querySelector('#bookList')
 
-
+addBookToMyLibrary("Brave New World", "Aldous Huxley", "311")
+addBookToMyLibrary("Nineteen Eighty-Four", "George Orwell", "328")
+addBookToMyLibrary("A Tale of Two Cities", "Charles Dickens", "448")
+appendToDomLibrary(myLibrary)
 
 
 // Object Constructor Function
@@ -12,6 +15,10 @@ function Book(title, author, pages) {
     this.read = "Unread"
     this.remove = "Remove"
 }
+
+const testArr = [1]
+testArr.splice(0, 1)
+console.log(testArr)
 
 // It is better to create a function in the prototype of an object. That way the function
 // is not recreated in all instances of the object, which matters if you have many instances
@@ -25,8 +32,13 @@ function addBookToMyLibrary(title, author, pages) {
     myLibrary.push(newBook)
 }
 
-// Update DOM Library, with an input library type of array containing objects
-function updateDomLibrary(lib) {
+// Remove all children elements from param node
+function removeAllChildNodes(parentNode) {
+    while (parentNode.firstChild) { parentNode.removeChild(parentNode.firstChild) }
+}
+
+// Append input library type of array containing objects to DOM Library
+function appendToDomLibrary(lib) {
     // Loop through the array of book objects in param library
     for (let i = 0; i < lib.length; i++) {
         const li = document.createElement('li')
@@ -42,20 +54,24 @@ function updateDomLibrary(lib) {
     }
 }
 
-addBookToMyLibrary("Brave New World", "Aldous Huxley", "311")
-addBookToMyLibrary("Nineteen Eighty-Four", "George Orwell", "328")
-addBookToMyLibrary("A Tale of Two Cities", "Charles Dickens", "448")
-
-
-updateDomLibrary(myLibrary)
-
-
-
 const removeBtns = document.querySelectorAll(".remove")
+const addBtn = document.querySelector(".addBtn")
 
-removeBtns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        const btnParentElement = e.target.parentElement;
-        btnParentElement.parentNode.removeChild(btnParentElement)
+// Assign addBook & removeBook functionality to all buttons
+removeBtns.forEach(btn => btn.addEventListener('click', removeBook));
+addBtn.addEventListener('click', addBook)
+
+// Remove button functionality
+function removeBook(e) {
+    myLibrary.forEach(libBook => {
+        if (libBook.title === e.target.parentNode.firstChild.textContent) {
+            e.target.parentNode.remove()
+            let libBookIndex = myLibrary.findIndex(libBook => libBook.title)
+            myLibrary.splice(libBookIndex, 1)
+        }
     })
-})
+}
+
+function addBook(e) {
+    if ()
+}
